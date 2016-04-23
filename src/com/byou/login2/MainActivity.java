7b0536da3c0466fd6349d7c3b14cc7327f1ad83c
lx <lxseven@hotmail.com>
2016-04-23 18:43:16 +0800

    first

---
+++ src/com/byou/login2/MainActivity.java
@@ -0,0 +1,37 @@
+package com.byou.login2;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button loginBtn = (Button) findViewById(R.id.button1);
+        OnClickListener listener = new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+
+				Toast.makeText(MainActivity.this, "»¶Ó­", Toast.LENGTH_LONG).show();
+			}
+		};
+		loginBtn.setOnClickListener(listener);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
